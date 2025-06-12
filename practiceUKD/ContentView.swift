926481785cc8ec371d70ad1919c3407afdd5f4b3
Viktor-Kizera <victorkizera2003@gmail.com>
2025-06-12 11:31:43 +0300

    Initial Commit

---
+++ practiceUKD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  practiceUKD
+//
+//  Created by Viktor Kizera on 6/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ practiceUKD/practiceUKDApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceUKDApp.swift
+//  practiceUKD
+//
+//  Created by Viktor Kizera on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct practiceUKDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
