050cd105b492c3487f7d62a93b1c73e2151b8cff
Viktor-Kizera <victorkizera2003@gmail.com>
2025-06-14 17:27:01 +0300

    added TabBar 0.2

---
+++ practiceUKD/Views/AddTransactionView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct AddTransactionView: View {
+    var body: some View {
+        Text("Додати дохід/витрату")
+            .font(.largeTitle)
+            .bold()
+            .padding()
+    }
+}
+
+#Preview {
+    AddTransactionView()
+} 
